d29261c9b89c8fd2d488662aba386bda7305ce38
Martin Valach <xvalac12@stud.fit.vut.cz>
2023-03-28 16:32:23 +0200

    feat: Added event for arrival of packet
    In event for packet arrival from network interface is basic print on stdin.

--- Sniffer.cs
+++ Sniffer.cs
@@ -11,19 +11,25 @@ namespace Network_sniffer
             CaptureDeviceList network_interfaces = CaptureDeviceList.Instance;
 
             Console.WriteLine("");
-            foreach(ICaptureDevice network_interface in network_interfaces)
+            foreach(var network_interface in network_interfaces)
             {
                 Console.WriteLine(network_interface.Name);
             }
             Console.WriteLine("");
 
+            var used_interface = network_interfaces[0];
 
-
-
-
-
-
-
+            used_interface.OnPacketArrival += (sender, packet) =>
+            {
+                Console.WriteLine("timestamp:");
+                Console.WriteLine("src MAC:");
+                Console.WriteLine("dst MAC:");
+                Console.WriteLine("frame lenght:");
+                Console.WriteLine("src IP:");
+                Console.WriteLine("dst IP:");
+                Console.WriteLine("src port:");
+                Console.WriteLine("dst port:");
+            };
 
         }
     }
