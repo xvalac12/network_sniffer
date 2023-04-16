8a2a83c13e6403a22aa2e79c7f52286166357a55
Martin Valach <xvalac12@stud.fit.vut.cz>
2023-04-16 13:08:05 +0200

    feat: Added usage to Error class
    When error is printed, it will print usage of application as well.

--- Error.cs
+++ Error.cs
@@ -14,10 +14,14 @@ namespace Network_sniffer
                 case 1:
                     Console.Error.WriteLine("Wrong argument entered");
                     break;
+                case 5:
+                    Console.Error.WriteLine("Wrong or no interface name entered");
+                    break;
                 case 99:
                     Console.Error.WriteLine("Internal Error");
                     break;
             }
+            Console.Error.WriteLine("USAGE: ./ipk-sniffer [-i interface | --interface interface] {-p port [--tcp|-t] [--udp|-u]} [--arp] [--icmp4] [--icmp6] [--igmp] [--mld] [--ndp] {-n num}");
             Environment.Exit(error_code);
         }
     }
