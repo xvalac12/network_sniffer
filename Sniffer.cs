d635024305df75b851e8fcea8bfb51857b4705c5
Martin Valach <xvalac12@stud.fit.vut.cz>
2023-04-17 14:08:24 +0200

    refactor: Changed filtering of packets
    Captured packets no longer use build-in SharpPcap filter. Method protocol_filter is used for it now.
    
    MLD filter work

--- Sniffer.cs
+++ Sniffer.cs
@@ -1,5 +1,12 @@
 ﻿using SharpPcap;
 using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Numerics;
+using System.Globalization;
 
 namespace Network_sniffer
 {
@@ -13,7 +20,7 @@ namespace Network_sniffer
         /// </summary>
         /// <param name="args">Command line arguments from user</param>
         /// <returns>Values for filtering and handling packets</returns>
-        static (string, string?, int, bool) argument_handling(string[] args)
+        static (string [], string?, int, bool) argument_handling(string[] args)
         {
             // check for duplicate in arguments
             if (args.Length != args.Distinct().Count())
@@ -29,8 +36,7 @@ namespace Network_sniffer
             string [] filter_arr = new string[10];
             int filter_cnt = 0;
             bool print_flag = true;
-            string mld = "icmp6[icmp6type] = icmp6-multicastlistenerreportv1 or icmp6[icmp6type] = icmp6-multicastlistenerreportv2 or icmp6[icmp6type] = icmp6-multicastlistenerquery or icmp6[icmp6type] = icmp6-multicastlistenerdone";
-            string ndp = "icmp6[icmp6type] = icmp6-neighborsolicit or icmp6[icmp6type] = icmp6-routersolicit or icmp6[icmp6type] = icmp6-routeradvert or icmp6[icmp6type] = icmp6-neighboradvert or icmp6[icmp6type] = icmp6-redirect";
+            string [] aux_filter_arr = {"arp", "tcp", "udp", "icmpv6", "icmpv4", "igmp", "ndp", "mld"};
             string? name_of_interface = null;
             int? port = null;
             int num_of_packets = 1;
@@ -81,11 +87,11 @@ namespace Network_sniffer
                         filter_cnt++;
                         break;
                     case "--icmpv4":
-                        filter_arr[filter_cnt] = "icmp";
+                        filter_arr[filter_cnt] = "icmpv4";
                         filter_cnt++;
                         break;
                     case "--icmpv6":
-                        filter_arr[filter_cnt] = "icmp6";
+                        filter_arr[filter_cnt] = "icmpv6";
                         filter_cnt++;
                         break;
                     case "--arp":
@@ -93,7 +99,7 @@ namespace Network_sniffer
                         filter_cnt++;
                         break;
                     case "--ndp":
-                        filter_arr[filter_cnt] = ndp;
+                        filter_arr[filter_cnt] = "ndp";
                         filter_cnt++;
                         break; 
                     case "--igmp":
@@ -101,7 +107,7 @@ namespace Network_sniffer
                         filter_cnt++;
                         break; 
                     case "--mld":
-                        filter_arr[filter_cnt] = mld;
+                        filter_arr[filter_cnt] = "mld";
                         filter_cnt++;
                         break;                     
                     case "-n":
@@ -127,23 +133,30 @@ namespace Network_sniffer
             {
                 Error.print_error(99);
             }
+
+            if (filter_arr.Length != filter_arr.Distinct().Count())
+            {
+                Error.print_error(7);
+            }
             
             if (port != null)
             {
                 filter_arr = port_handling(filter_arr, port);
             }
 
-            // Input between every element of filter 'or'
-            string filter = string.Join(" or ", filter_arr);
+            if (filter_arr.Length == 0)
+            {
+                return (aux_filter_arr, name_of_interface, num_of_packets, print_flag);
+            }
 
-            return (filter, name_of_interface, num_of_packets, print_flag);
+            return (filter_arr, name_of_interface, num_of_packets, print_flag);
         }
 
         /// <summary>
         /// Funtion print name of all avaible interfaces and exit application.
         /// </summary>
         /// <param name="network_interfaces">List with info about all avaible interfaces</param>
-        static void print_all_interfaces(CaptureDeviceList network_interfaces)
+        private static void print_all_interfaces(CaptureDeviceList network_interfaces)
         {
                 Console.WriteLine("");
                 foreach(var network_interface in network_interfaces)
@@ -160,23 +173,24 @@ namespace Network_sniffer
         /// </summary>
         /// <param name="filter_arr">Array with protocols for filtering</param>
         /// <param name="port">Port which will be used for TCP or UDP packet cpaturing</param>
-        /// <returns>String with filter for packet capturing</returns>
-        static string[] port_handling(string [] filter_arr, int? port)
+        /// <returns>String array with filter for packet capturing</returns>
+        private static string[] port_handling(string [] filter_arr, int? port)
         {
             var aux_flag = true;
-            for (int cnt = 0; cnt < filter_arr.Length; cnt++)
+            for (int filter_cnt = 0; filter_cnt < filter_arr.Length; filter_cnt++)
             {
-                if (filter_arr[cnt] == "udp")
+                if (filter_arr[filter_cnt] == "udp")
                 {
-                    filter_arr[cnt]  = "(udp and port " + port +")";
+                    filter_arr[filter_cnt]  = "udp" + port;
                     aux_flag = false;
                 }
-                else if (filter_arr[cnt]  == "tcp")
+                else if (filter_arr[filter_cnt]  == "tcp")
                 {
-                    filter_arr[cnt]  = "(tcp and port " + port +")";
+                    filter_arr[filter_cnt]  = "tcp" + port;
                     aux_flag = false;
-                }   
+                }
             }
+            
             if (aux_flag)
             {
                 Error.print_error(3);
@@ -199,31 +213,136 @@ namespace Network_sniffer
                 hexdump_line = hexdump_line.Remove(28, 1);
                 hexdump_line = hexdump_line.Remove(52, 1);
                 hexdump_line = hexdump_line.Remove(53, 1);
-                            
-                Console.WriteLine("0x" + hexdump_line);    
+
+                Console.WriteLine("0x" + hexdump_line);   
             }
         }
         
+        /// <summary>
+        /// Method used for filtering if captured packet is in the filter
+        /// </summary>
+        /// <param name="handled_packet">Captured packet</param>
+        /// <param name="filter_arr">Protocol filter from user</param>
+        /// <returns>True if captured packet is in filter, otherwise false</returns>
+        static bool protocol_filter(Packet handled_packet, string [] filter_arr)
+        {
+            bool correct_catch = false;
+            int? port = null;
+            var ip_packet = handled_packet.Extract<PacketDotNet.IPPacket>();
+
+            for (int filter_cnt = 0; filter_cnt < filter_arr.Length; filter_cnt++)
+            {
+                if (Regex.Match(filter_arr[filter_cnt], @"^(udp|tcp)(\d){1,5}$").Success)
+                {
+                    port = int.Parse(filter_arr[filter_cnt].Substring(3));
+                }
+
+                if(ip_packet == null && filter_arr [filter_cnt] == "arp")
+                {
+                    if(handled_packet.Extract<ArpPacket>() != null) correct_catch = true;
+                }
+                else if(ip_packet != null)
+                {
+                    switch (filter_arr [filter_cnt])
+                    {
+                        case "tcp":
+                            var tcp_packet = ip_packet.Extract<TcpPacket>();
+                            if(tcp_packet != null)
+                            {
+                                if (port == null)
+                                {
+                                    correct_catch = true;
+                                }
+                                else if (tcp_packet.SourcePort == port || tcp_packet.DestinationPort == port)
+                                {
+                                    correct_catch = true;
+                                }
+                            } 
+                            break;
+                        case "udp":
+                            var udp_packet = ip_packet.Extract<TcpPacket>();
+                            if(udp_packet != null)
+                            {
+                                if (port == null)
+                                {
+                                    correct_catch = true;
+                                }
+                                else if (udp_packet.SourcePort == port || udp_packet.DestinationPort == port)
+                                {
+                                    correct_catch = true;
+                                }
+                            } 
+                            break;
+                        case "icmpv4":
+                            if(ip_packet.Extract<IcmpV4Packet>() != null) correct_catch = true;
+                            break;
+                        case "icmpv6":
+                            if(ip_packet.Extract<IcmpV6Packet>() != null) correct_catch = true;
+                            break;
+                        case "mld":
+                            if(ip_packet.Extract<IcmpV6Packet>() != null)
+                            {
+                                switch(ip_packet.Extract<IcmpV6Packet>().Type)
+                                {
+                                    case IcmpV6Type.MulticastListenerQuery:             //130
+                                    case IcmpV6Type.MulticastListenerReport:            //131
+                                    case IcmpV6Type.MulticastListenerDone:              //132
+                                    case IcmpV6Type.Version2MulticastListenerReport:    //144
+                                        correct_catch = true;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            break;
+                        case "ndp":
+                            if(ip_packet.Extract<IcmpV6Packet>() != null)
+                            {
+                                if(ip_packet.Extract<IcmpV6Packet>() != null)
+                                {
+                                    switch(ip_packet.Extract<IcmpV6Packet>().Type)
+                                    {
+                                        case IcmpV6Type.RouterSolicitation:             //133
+                                        case IcmpV6Type.RouterAdvertisement:            //134
+                                        case IcmpV6Type.NeighborSolicitation:           //135
+                                        case IcmpV6Type.NeighborAdvertisement:          //136
+                                        case IcmpV6Type.RedirectMessage:                //137
+                                            correct_catch = true;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                            }
+                            break;  
+                        case "igmp":
+                            if(ip_packet.Extract<IgmpPacket>() != null) {correct_catch = true;}
+                            break;
+                    }
+                }
+                if (correct_catch) return true;
+            }
+            return false;   
+        }
+
+
         /// <summary>
         /// Method for extracting data from packet, for information about packet print.
         /// It counts number od handled packet as well 
         /// </summary>
         /// <param name="sender">Object</param>
         /// <param name="packet">Captured packet</param>
-        /// <param name="packet_counter">Current captured packet</param>
-        /// <param name="num_of_packets">Number of packets to be captured</param>
         /// <param name="used_interface">Info about sniffed interface</param>
         /// <returns>True if packet was handled, false if not</returns>
-        private static bool packet_handling(object sender, PacketCapture packet, ILiveDevice used_interface)
+        private static bool packet_handling(object sender, PacketCapture packet, string [] filter_arr, ILiveDevice used_interface)
         {
-
             var handled_packet = Packet.ParsePacket(packet.GetPacket().LinkLayerType, packet.GetPacket().Data);
             var eth_packet = handled_packet.Extract<PacketDotNet.EthernetPacket>();
             var ip_packet = handled_packet.Extract<PacketDotNet.IPPacket>();
-            var tcpPacket = handled_packet.Extract<PacketDotNet.TcpPacket>();                
-            var udpPacket = handled_packet.Extract<PacketDotNet.UdpPacket>();
+            var tcp_packet = handled_packet.Extract<PacketDotNet.TcpPacket>();                
+            var udp_packet = handled_packet.Extract<PacketDotNet.UdpPacket>();
 
-            if (eth_packet != null)
+            if (eth_packet != null && protocol_filter(handled_packet, filter_arr))
             {
                 var date = packet.Header.Timeval.Date;  
                 var source_MAC = "src MAC: " + eth_packet.SourceHardwareAddress;
@@ -246,17 +365,17 @@ namespace Network_sniffer
                     Console.WriteLine("dst IP: " + ip_packet.DestinationAddress);
                 }
 
-                if (tcpPacket != null || udpPacket != null)
+                if (tcp_packet != null || udp_packet != null)
                 {
-                    if (tcpPacket != null)
+                    if (tcp_packet != null)
                     {
-                        Console.WriteLine("src port: " + tcpPacket.SourcePort);
-                        Console.WriteLine("dst port: " + tcpPacket.DestinationPort);
+                        Console.WriteLine("src port: " + tcp_packet.SourcePort);
+                        Console.WriteLine("dst port: " + tcp_packet.DestinationPort);
                     }
                     else
                     {
-                        Console.WriteLine("src port: " + udpPacket.SourcePort);
-                        Console.WriteLine("dst port: " + udpPacket.DestinationPort);       
+                        Console.WriteLine("src port: " + udp_packet.SourcePort);
+                        Console.WriteLine("dst port: " + udp_packet.DestinationPort);       
                     }
                 }   
                 Console.WriteLine("");
@@ -274,14 +393,14 @@ namespace Network_sniffer
         /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
-            string protocol_filter;
+            string [] string_arr;
             string? name_of_interface;
             int num_of_packets;
             bool print_flag;
             int packet_cnt = 0;
             ILiveDevice? used_interface = null;
             CaptureDeviceList network_interfaces = CaptureDeviceList.Instance;
-            (protocol_filter, name_of_interface, num_of_packets, print_flag) = argument_handling(args);
+            (string_arr, name_of_interface, num_of_packets, print_flag) = argument_handling(args);
 
             if (print_flag == true) print_all_interfaces(network_interfaces);
             
@@ -307,18 +426,10 @@ namespace Network_sniffer
             {
                 Error.print_error(6);
             }
-            try
-            {
-                used_interface.Filter = protocol_filter;
-            }
-            catch
-            {
-                Error.print_error(99);
-            }
 
             used_interface.OnPacketArrival += (sender, packet) =>
             { 
-                if (packet_handling(sender, packet, used_interface))
+                if (packet_handling(sender, packet, string_arr, used_interface))
                 {
                     if (++packet_cnt >= num_of_packets)
                     {
